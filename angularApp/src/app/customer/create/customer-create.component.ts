a3b5006437317f9bfce0293a42ed2beda9c038b3
Karan S <88817564+KaranS-hexaware@users.noreply.github.com>
2023-03-15 20:44:16 +0530

    Web app

---
+++ angularApp/src/app/customer/create/customer-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-create',
+  templateUrl: './customer-create.component.html',
+  styleUrls: ['./customer-create.component.css']
+})
+export class CustomerCreateComponent {
+}
